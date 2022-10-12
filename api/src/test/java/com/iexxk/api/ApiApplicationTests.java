64051e830902e7364c5c437341b2aa2bbe01cd2f
xuanleung <exxk.lx@gmail.com>
2022-10-12 10:53:48 +0800

    Initial commit

---
+++ api/src/test/java/com/iexxk/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iexxk.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ service/src/test/java/com/iexxk/service/ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iexxk.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
